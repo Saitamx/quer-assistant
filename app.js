bc275306da17b5417790ceb626dda7ee5f73d828
Matias Troncoso <matias.troncoso.campos@gmail.com>
2023-12-03 08:29:04 -0300

    v1 for chat_bithia_maty

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+// Importando los módulos necesarios
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const functions = require("./functions");
 require("dotenv").config();
 
+// Creando la aplicación Express y configurando el middleware
 const app = express();
 app.use(bodyParser.json());
 
+// Configurando la clave de API de OpenAI y la instancia de Axios
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const axiosInstance = axios.create({
   headers: {
@@ -17,11 +20,15 @@ const axiosInstance = axios.create({
   maxBodyLength: Infinity,
 });
 
+// Variable para almacenar el ID del asistente
 let assistantId;
+
+// Creando un asistente de OpenAI y almacenando su ID
 functions.createAssistant(axiosInstance).then((assistantIdReturned) => {
   assistantId = assistantIdReturned;
 });
 
+// Ruta para iniciar una nueva conversación
 app.get("/start", async (req, res) => {
   try {
     const response = await axiosInstance.post(
@@ -36,9 +43,10 @@ app.get("/start", async (req, res) => {
   }
 });
 
+// Ruta para manejar el chat
 app.post("/chat", async (req, res) => {
   const { thread_id, message } = req.body;
-  console.log("req.body", req.body);
+
   if (!thread_id) {
     console.log("Error: Missing thread_id");
     return res.status(400).json({ error: "Missing thread_id" });
@@ -49,60 +57,54 @@ app.post("/chat", async (req, res) => {
       `https://api.openai.com/v1/threads/${thread_id}/messages`,
       { role: "user", content: message }
     );
+
     const response = await axiosInstance.post(
       `https://api.openai.com/v1/threads/${thread_id}/runs`,
       { assistant_id: assistantId }
     );
     console.log(`Run initiated with ID: ${response.data.id}`);
-    res.json({ run_id: response.data.id, status: "in_progress" });
+    console.log({ run_id: response.data.id, status: "in_progress" });
+
+    handleResponseInBackground(thread_id, response.data.id, res);
   } catch (error) {
     console.error("Error in chat:", error.response || error);
     res.status(500).send("Error in chat");
   }
 });
 
-app.get("/chat/status/:run_id/:thread_id", async (req, res) => {
-  const { run_id, thread_id } = req.params;
-  try {
-    const runStatusResponse = await axiosInstance.get(
-      `https://api.openai.com/v1/threads/${thread_id}/runs/${run_id}`
-    );
-    const runStatus = runStatusResponse.data.status;
+// Función para manejar la respuesta en segundo plano
+async function handleResponseInBackground(thread_id, run_id, res) {
+  let runStatus;
+  do {
+    try {
+      // Ejecutando consultas en paralelo
+      const [statusResponse, messagesResponse] = await Promise.all([
+        axiosInstance.get(
+          `https://api.openai.com/v1/threads/${thread_id}/runs/${run_id}`
+        ),
+        axiosInstance.get(
+          `https://api.openai.com/v1/threads/${thread_id}/messages`
+        ),
+      ]);
+      runStatus = statusResponse.data.status;
 
-    if (runStatus === "completed") {
-      const messagesResponse = await axiosInstance.get(
-        `https://api.openai.com/v1/threads/${thread_id}/messages`
-      );
-      const assistantResponses = messagesResponse.data.data
-        .filter((message) => message.role === "assistant")
-        .map((msg) =>
-          msg.content && msg.content.length > 0
-            ? msg.content[0].text
-            : "No text content"
-        );
-
-      res.json({ status: runStatus, response: assistantResponses });
-    } else {
-      res.json({ status: runStatus });
+      // Si la ejecución está completada, enviar la respuesta
+      if (runStatus === "completed") {
+        res.status(200).json({ messages: messagesResponse.data });
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Error retrieving response");
+      return;
     }
-  } catch (error) {
-    console.error("Error checking chat status:", error.response || error);
-    res.status(500).send("Error checking chat status");
-  }
-});
 
-app.get("/models", async (req, res) => {
-  try {
-    const response = await axiosInstance.get(
-      "https://api.openai.com/v1/models"
-    );
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error retrieving models:", error);
-    res.status(500).send("Error retrieving models");
-  }
-});
+    // Esperando antes de la próxima verificación
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  } while (runStatus !== "completed");
+}
 
+// Configurando el puerto y poniendo en marcha el servidor
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const FormData = require("form-data");
+const axios = require("axios");
 
 const createAssistant = async (axiosInstance) => {
   const assistantFilePath = path.join(__dirname, "assistant.json");
@@ -31,7 +32,7 @@ const createAssistant = async (axiosInstance) => {
       "https://api.openai.com/v1/assistants",
       {
         instructions:
-          "Desarrolla un asistente de inteligencia artificial para la disciplina deportiva Caliwea, que promueva la alegría del movimiento, la diversidad de experiencias, y la construcción de una comunidad inclusiva y respetuosa. El asistente debe enfocarse en la seguridad, el bienestar integral, y ofrecer un sistema de gamificación para motivar a los usuarios. Debe integrar elementos de calistenia, escalada, yoga, y celebrar la cultura chilena, fomentando la participación activa en una comunidad vibrante y alegre. También debe promover la educación y la cultura chilena en la comunidad. El asistente debe tener una comunicación concreta, fluida y efectiva, y debe responder a las preguntas y solicitudes de manera clara y concisa. El asistente debe tener una estructura clara y eficiente para responder a las preguntas y solicitudes. Hemos proporcionado un documento que contiene informacion de caliwea, cada vez que no sepas algo, recurre al documento....",
+          "Desarrolla un asistente de inteligencia artificial para la disciplina deportiva Caliwea, que promueva la alegría del movimiento, la diversidad de experiencias, y la construcción de una comunidad inclusiva y respetuosa. El asistente debe enfocarse en la seguridad, el bienestar integral, y ofrecer un sistema de gamificación para motivar a los usuarios. Debe integrar elementos de calistenia, escalada, yoga, y celebrar la cultura chilena, fomentando la participación activa en una comunidad vibrante y alegre. También debe promover la educación y la cultura chilena en la comunidad. El asistente debe tener una comunicación concreta, fluida y efectiva, y debe responder a las preguntas y solicitudes de manera clara y concisa. El asistente debe tener una estructura clara y eficiente para responder a las preguntas y solicitudes. Hemos proporcionado un documento que contiene informacion de caliwea, cada vez que no sepas algo respecto a tu contexto principal, recurre al documento.",
         model: "gpt-4-1106-preview",
         tools: [{ type: "retrieval" }],
         file_ids: [fileResponse.data.id],
