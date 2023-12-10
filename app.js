95f0de3a2e5eeb5ca7a95f9ed61249a90df4dbdc
Saitamx <59573760+Saitamx@users.noreply.github.com>
2023-12-10 14:04:45 +0000

    restaurant system with llm

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// Importando los módulos necesarios
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const functions = require("./functions");
 require("dotenv").config();
 
-// Creando la aplicación Express y configurando el middleware
 const app = express();
 app.use(bodyParser.json());
 
-// Configurando la clave de API de OpenAI y la instancia de Axios
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const axiosInstance = axios.create({
   headers: {
@@ -20,131 +17,54 @@ const axiosInstance = axios.create({
   maxBodyLength: Infinity,
 });
 
-// Variable para almacenar el ID del asistente
-let assistantId;
+let assistantId = 'asst_hqofASahTlXspPHZYriRY8PU';
 
-// Creando un asistente de OpenAI y almacenando su ID
-functions.createAssistant(axiosInstance).then((assistantIdReturned) => {
-  assistantId = assistantIdReturned;
-});
+app.post("/newThread", async (req, res) => {
+  const { user, password } = req.body;
 
-// Ruta para iniciar una nueva conversación
-app.get("/start", async (req, res) => {
+  if (!user || !password) {
+    return res.status(400).json({ error: "Faltan campos requeridos." });
+  }
+
+  if (user !== process.env.USER || password !== process.env.PASSWORD) {
+    return res.status(403).json({ error: "Autenticación fallida." });
+  }
+
   try {
-    const response = await axiosInstance.post(
-      "https://api.openai.com/v1/threads",
-      { messages: [] }
-    );
-    console.log(`New thread created with ID: ${response.data.id}`);
-    res.json({ thread_id: response.data.id });
+    assistantId = await functions.handleCreateAssistant(axiosInstance);
+    console.log(`New assistant created with ID: ${assistantId}`);
+    res.json({ assistantId });
   } catch (error) {
-    console.error("Error starting a new conversation:", error);
-    res.status(500).send("Error starting a new conversation");
+    console.error("Error creating assistant:", error);
+    res.status(500).send("Error creating assistant");
   }
 });
 
-// Ruta para manejar el chat
-app.post("/chat", async (req, res) => {
-  const { thread_id, message } = req.body;
+app.post("/newMessage", async (req, res) => {
+  const { message } = req.body;
 
-  if (!thread_id) {
-    console.log("Error: Missing thread_id");
-    return res.status(400).json({ error: "Missing thread_id" });
-  }
+  let thread_id = 'thread_BxYTAKAE4hiU8Q5ZKy18QmuM'
 
   try {
-    const colorResponse = await handleClassifyQuestion(message);
-
-    await axiosInstance.post(
-      `https://api.openai.com/v1/threads/${thread_id}/messages`,
-      { role: "user", content: message }
-    );
+    const categoryCode = await functions.handleClassifyQuestion(message);
 
     const response = await axiosInstance.post(
       `https://api.openai.com/v1/threads/${thread_id}/runs`,
-      { assistant_id: assistantId }
+      { assistant_id: assistantId},
     );
     console.log(`Run initiated with ID: ${response.data.id}`);
 
-    const messagesResponse = await handleResponseInBackground(
+    const messages = await functions.handleResponseInBackground(
       thread_id,
-      response.data.id
+      response.data.id,
     );
-    res.status(200).json({ messages: messagesResponse, color: colorResponse });
+    res.status(200).json({ messages, categoryCode });
   } catch (error) {
-    console.error("Error in chat:", error.response || error);
+    console.error("Error in chat:", error.response.data.error);
     res.status(500).send("Error in chat");
   }
 });
 
-// Función para manejar la respuesta en segundo plano
-async function handleResponseInBackground(thread_id, run_id) {
-  let runStatus;
-  let attempts = 0;
-  const maxAttempts = 12;
-  const interval = 2500;
-
-  do {
-    try {
-      const [statusResponse, messagesResponse] = await Promise.all([
-        axiosInstance.get(
-          `https://api.openai.com/v1/threads/${thread_id}/runs/${run_id}`
-        ),
-        axiosInstance.get(
-          `https://api.openai.com/v1/threads/${thread_id}/messages`
-        ),
-      ]);
-      runStatus = statusResponse.data.status;
-
-      if (runStatus === "completed") {
-        return messagesResponse.data;
-      }
-    } catch (error) {
-      console.error("Error retrieving response:", error);
-      return { error: "Error retrieving response" };
-    }
-    await new Promise((resolve) => setTimeout(resolve, interval));
-    attempts++;
-  } while (runStatus !== "completed" && attempts < maxAttempts);
-
-  if (attempts >= maxAttempts) {
-    return { error: "Timeout: La respuesta del asistente tardó demasiado." };
-  }
-}
-
-// Clasificación de preguntas con modelo llm
-const handleClassifyQuestion = async (question) => {
-  const prompt = `Pregunta: "${question}"\n La pregunta anterior, ¿esta relacionada con alguno de estos 4 colores?: azul, rojo, rosado, amarillo. Responde en una sola palabra el color que creas que esta relacionado con la pregunta anterior.`;
-  let response = await axios.post(
-    process.env.CHAT_SERVICE,
-    {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
-      temperature: 0.0,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  response = response.data?.choices[0]?.message["content"].toLowerCase();
-  return response === "azul" ||
-    response === "rojo" ||
-    response === "rosado" ||
-    response === "amarillo"
-    ? response
-    : false;
-};
-
-// Configurando el puerto y poniendo en marcha el servidor
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- functions.js
+++ functions.js
@@ -3,18 +3,31 @@ const path = require("path");
 const FormData = require("form-data");
 const axios = require("axios");
 
-const createAssistant = async (axiosInstance) => {
+// Configuración de Axios
+const axiosInstance = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    "OpenAI-Beta": "assistants=v1",
+  },
+  maxBodyLength: Infinity,
+});
+
+const handleCreateAssistant = async () => {
   const assistantFilePath = path.join(__dirname, "assistant.json");
 
   if (fs.existsSync(assistantFilePath)) {
     const assistantData = JSON.parse(
-      fs.readFileSync(assistantFilePath, "utf8")
+      fs.readFileSync(assistantFilePath, "utf8"),
     );
     console.log("Loaded existing assistant ID.");
     return assistantData.assistant_id;
   } else {
     const fileFormData = new FormData();
-    fileFormData.append("file", fs.createReadStream("caliwea.docx"));
+    fileFormData.append(
+      "file",
+      fs.createReadStream("knowledge_restaurant.docx"),
+    );
     fileFormData.append("purpose", "assistants");
 
     const fileResponse = await axiosInstance.post(
@@ -25,29 +38,105 @@ const createAssistant = async (axiosInstance) => {
           ...fileFormData.getHeaders(),
           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         },
-      }
+      },
     );
 
     const assistantResponse = await axiosInstance.post(
       "https://api.openai.com/v1/assistants",
       {
         instructions:
-          "Desarrolla un asistente de inteligencia artificial para la disciplina deportiva Caliwea, que promueva la alegría del movimiento, la diversidad de experiencias, y la construcción de una comunidad inclusiva y respetuosa. El asistente debe enfocarse en la seguridad, el bienestar integral, y ofrecer un sistema de gamificación para motivar a los usuarios. Debe integrar elementos de calistenia, escalada, yoga, y celebrar la cultura chilena, fomentando la participación activa en una comunidad vibrante y alegre. También debe promover la educación y la cultura chilena en la comunidad. El asistente debe tener una comunicación concreta, fluida y efectiva, y debe responder a las preguntas y solicitudes de manera clara y concisa. El asistente debe tener una estructura clara y eficiente para responder a las preguntas y solicitudes. Hemos proporcionado un documento que contiene informacion de caliwea, cada vez que no sepas algo respecto a tu contexto principal, recurre al documento.",
-        model: "gpt-4-1106-preview",
+          "Eres un asistente digital avanzado diseñado para ayudar en restaurantes. ...",
+        model: "gpt-3.5-turbo-1106",
         tools: [{ type: "retrieval" }],
         file_ids: [fileResponse.data.id],
-      }
+      },
     );
 
     const assistantId = assistantResponse.data.id;
 
     fs.writeFileSync(
       assistantFilePath,
-      JSON.stringify({ assistant_id: assistantId }, null, 2)
+      JSON.stringify({ assistant_id: assistantId }, null, 2),
     );
     console.log("Created a new assistant and saved the ID.");
     return assistantId;
   }
 };
 
-module.exports = { createAssistant };
+const handleClassifyQuestion = async (question) => {
+  const messages = [
+    {
+      role: "system",
+      content:
+      "Clasifica las preguntas de los clientes en una de las siguientes categorías, respondiendo con el número correspondiente: 1 para 'Menú y Opciones de Comida', 2 para 'Precios y Promociones', 3 para 'Reservas y Disponibilidad de Mesas', 4 para 'Políticas y Servicios Adicionales'. Solo debes responder con el número de la categoría correspondiente.",
+    },
+    {
+      role: "user",
+      content: question,
+    },
+  ];
+
+  const response = await axiosInstance.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages,
+      temperature: 0.0,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  const predictedCategory = response.data.choices[0].message.content.trim();
+  const categoryCode = ["1", "2", "3", "4"].includes(predictedCategory)
+    ? predictedCategory
+    : "0";
+
+  console.log(`Categoría detectada: ${categoryCode}`);
+  return categoryCode;
+};
+
+async function handleResponseInBackground(thread_id, run_id) {
+  let runStatus;
+  let attempts = 0;
+  const maxAttempts = 12;
+  const interval = 2500;
+
+  do {
+    try {
+      const [statusResponse, messagesResponse] = await Promise.all([
+        axiosInstance.get(
+          `https://api.openai.com/v1/threads/${thread_id}/runs/${run_id}`,
+        ),
+        axiosInstance.get(
+          `https://api.openai.com/v1/threads/${thread_id}/messages`,
+        ),
+      ]);
+
+      runStatus = statusResponse.data.status;
+      if (runStatus === "completed") {
+        return messagesResponse.data;
+      }
+    } catch (error) {
+      console.error("Error retrieving response:", error);
+      return { error: "Error retrieving response" };
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, interval));
+    attempts++;
+  } while (runStatus !== "completed" && attempts < maxAttempts);
+
+  if (attempts >= maxAttempts) {
+    return { error: "Timeout: La respuesta del asistente tardó demasiado." };
+  }
+}
+
+module.exports = {
+  handleCreateAssistant,
+  handleClassifyQuestion,
+  handleResponseInBackground,
+};
